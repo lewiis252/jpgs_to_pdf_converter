70f807b7f8e6a6bb1397787d3b12c7bbc0526a2d
lewiis252 <krystianoleniacz@gmail.com>
2022-10-13 19:25:37 +0200

    rotating do not work well

--- converter.py
+++ converter.py
@@ -2,7 +2,6 @@ from PIL import Image
 from os import listdir
 from os.path import isfile, join
 
-rotate_rate = int(input('Please enter degree of rotate (integer): '))
 name_of_file = input('Please enter name of pdf you want to create: ')
 
 # get names of all files in folder
@@ -13,11 +12,10 @@ to_convert = []
 # convert images
 for image in images[1:]:
     im = Image.open(fr'files_to_convert\{image}')
-    im = im.rotate(rotate_rate) # rotate image if needed
 
     to_convert.append(im.convert('RGB'))
 
-image_1 = Image.open(fr'files_to_convert\{images[0]}').rotate(rotate_rate)
+image_1 = Image.open(fr'files_to_convert\{images[0]}')
 im_1 = image_1.convert('RGB')
 
 # make pdf

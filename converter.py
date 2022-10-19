a7bd3413c806cc020218f2fa0348e75dde02367d
lewiis252 <krystianoleniacz@gmail.com>
2022-10-19 13:08:11 +0200

    change paths

--- converter.py
+++ converter.py
@@ -6,17 +6,18 @@ name_of_file = input('Please enter name of pdf you want to create: ')
 
 # get names of all files in folder
 
-images = [f for f in listdir('files_to_convert') if isfile(join('files_to_convert', f))]
+images = [fr'files_to_convert/{f}' for f in listdir('files_to_convert') if isfile(join('files_to_convert', f))]
 to_convert = []
 
 # convert images
 for image in images[1:]:
-    im = Image.open(fr'files_to_convert\{image}')
+    im = Image.open({image})
 
     to_convert.append(im.convert('RGB'))
 
-image_1 = Image.open(fr'files_to_convert\{images[0]}')
+image_1 = Image.open(images[0])
 im_1 = image_1.convert('RGB')
 
 # make pdf
 im_1.save(fr'{name_of_file}.pdf', save_all=True, append_images=to_convert)
+
